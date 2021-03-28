643d9bcda71384f22b0910d94e62d2f98e1ed619
ColbyWTaylor <colbyt.093@gmail.com>
2021-03-28 18:11:24 -0400

    incremental operators and namechecking function examples

--- 02_variables.php
+++ 02_variables.php
@@ -5,13 +5,17 @@
 // Variable types
 
 // Declare variables
-
+$name = "Colby";
+$year = 2021;
 // Print the variables. Explain what is concatenation
-
+echo "<h1>".$name."</h1><br>";
+echo $year;
+echo "<p>".$name." is a ".gettype($name)."</p>";
 // Print types of the variables
+var_dump($name, $year);
 
 // Print the whole variable
-
+ is_string($name);
 // Change the value of the variable
 
 // Print type of the variable
@@ -20,6 +24,10 @@
 
 // Check if variable is defined
 
+isset($name);
+
 // Constants
+define('PI', 3.14);
+echo PI.'<br>';
 
 // Using PHP built-in constants
--- 03_numbers.php
+++ 03_numbers.php
@@ -1,17 +1,31 @@
 <?php
 
 // Declaring numbers
-
-// Arithmetic operations
+$a = 5;
+$b = 4;
+$c = 1.2;
+// Arithmetic operations, standard order of operations
+// echo ($a + $b) * $c;
+// echo ($a + $b) * $c . '<br>';
+// echo $a -= $b . '<br>';
+// echo $a *= $b . '<br>';
+// echo $a /= $b . '<br>';
+// echo $a %= $b . '<br>';
 
 // Assignment with math operators
-
+// $a += $b; echo $a.'<br>';
 // Increment operator
-
+echo $a++ .'<br>'; // value of a is printed and then increased by 1
+echo ++$a .'<br>'; // value of a is increased and then printed   
 // Decrement operator
-
+echo $a-- .'<br>';
+echo --$a . '<br>';
 // Number checking functions
-
+is_float(1.24); // true
+is_double(1.25); // true
+is_int(5); // true
+is_numeric("3.45"); // true
+is_numeric("3.45ssdfg"); // false
 // Conversion
 
 // Number functions
