6a4dd760c432eb949f5583e86c7d693f0164cf60
ColbyWTaylor <colbyt.093@gmail.com>
2021-03-29 23:36:00 -0400

    numerical functions

--- 03_numbers.php
+++ 03_numbers.php
@@ -31,8 +31,15 @@ $strNumber = '12.34';
 // $number = (float)$strNumber;
 $number = (int)$strNumber;
 var_dump($number);
+echo '<br>';
 // Number functions
-
+echo "abs(-15) " . abs(-15) . '<br>'; 
+echo "pow(2,3) " . pow(2,3) . '<br>'; 
+echo "sqrt(16) " . sqrt(16) . '<br>'; 
+echo "max(2,3) " . max(2,3) . '<br>'; 
+echo "min(2,3) " . min(2,3) . '<br>'; 
+echo "round(2.4) " . round(2.4) . '<br>'; 
+echo "round(2.5) " . round(2.5) . '<br>'; 
 // Formatting numbers
 
 // https://www.php.net/manual/en/ref.math.php
