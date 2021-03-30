57c353683b01534535d07382f6818cdc7d80c3d0
ColbyWTaylor <colbyt.093@gmail.com>
2021-03-29 23:55:03 -0400

    strings

--- 04_strings.php
+++ 04_strings.php
@@ -1,8 +1,14 @@
 <?php
 
 // Create simple string
-
+$name = 'Godzilla';
+$string = 'Hello .$name I am always 42 years old'; // singe quotes just string
+$string2 = "Hello $name I am always 42 years old"; // double quotes can use variables
+echo $string.'<br>';
+echo $string2.'<br>';
 // String concatenation
+
+echo 'Hello'.' World'.' and PHP'.'<br>' ;
 
 // String functions
 
